4b67587ac5ee17d5f2ba885b514d92bc152049f8
Lakshmi <lakshmidevy.easwaran@gmail.com>
2019-01-18 11:15:04 +0530

    SOP added

--- hellogit.java
+++ hellogit.java
@@ -1,5 +1,7 @@
 class hellogit{
     public static void main(String args[]){
         System.out.println("HELLO GIT");
+         System.out.println("Welcome");
+          System.out.println("Spring");
     }
 }
